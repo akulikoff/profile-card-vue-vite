47e0a623b2884b55d4132b9c22d720dbd8f781d0
akulikoff <akulikoff86@gmail.com>
2024-05-21 22:25:08 +1000

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TestimonialCard from "./components/TestimonialCard.vue";
+const data = {
+  fullName: "Sara Dole",
+  linkBio: "@saradole",
+  summary:
+    "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!",
+  imageUrl: "/profile-thumbnail.png",
+};
+</script>
+
+<template>
+  <TestimonialCard v-bind="data" />
+</template>
+
+<style scoped></style>
---
+++ src/components/TestimonialCard.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+interface CardProps {
+  fullName: string;
+  linkBio: string;
+  summary: string;
+  imageUrl: string;
+}
+defineProps<CardProps>();
+</script>
+
+<template>
+  <body class="bg-gradient-to-b from-grey-50 to-grey-300">
+    <div class="card flex flex-col justify-center px-4 mx-auto">
+      <article class="bg-white rounded-lg p-4 shadow-lg">
+        <div class="my-4 flex flex-row">
+          <img
+            :src="imageUrl"
+            alt="profile image"
+            loading="lazy"
+            class="h-12 w-12 rounded-full"
+          />
+          <div class="flex flex-col ml-4">
+            <h3 class="text-lg text-neutral-900 font-semibold line-clamp-1">
+              {{ fullName }}
+            </h3>
+            <span class="text-sm text-neutral-600 font-medium">{{
+              linkBio
+            }}</span>
+          </div>
+        </div>
+        <p class="text-neutral-600 mb-2 font-medium text-base line-clamp-5">
+          {{ summary }}
+        </p>
+      </article>
+    </div>
+  </body>
+</template>
+
+<style scoped>
+body {
+  margin: 0;
+  font-family: "Noto Sans", sans-serif;
+  min-height: 100vh;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-rendering: optimizeLegibility;
+}
+
+.card {
+  width: 372px;
+  margin-top: 120px;
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>
